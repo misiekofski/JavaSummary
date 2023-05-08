4aa34acadad434b2fcb4a67814fd055297c51a1b
Michal Dobrzycki <dobrzycki@gmail.com>
2023-05-08 19:21:08 +0200

    Fixes

--- src/main/java/hr/App.java
+++ src/main/java/hr/App.java
@@ -4,28 +4,4 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class App {
-    public static void main(String[] args) {
-//        System.out.println("Kadry...");
-//        List<IEmployable> peopleList = new ArrayList<>();
-//
-//        // let's create new different types of employees
-//        Teacher t1 = new Teacher("Jan", "Kowalski", 6666);
-//        Teacher t2 = new Teacher("Grazyna", "Nowak", 7777);
-//        Manager m1 = new Manager("Michal", "Menadżerski", 0.2);
-//
-//        peopleList.add(t1);
-//        peopleList.add(t2);
-//        peopleList.add(m1);
-//
-//        double howMuchDoWePay = 0;
-//
-//        for (IEmployable p : peopleList) {
-//            // yearly wages of people working in our company, one by one
-//            System.out.println(p.getPersonFullName() + " gets " + p.getYearlyWage() + " yearly ");
-//            // add each wage to total amount
-//            howMuchDoWePay += p.getYearlyWage();
-//        }
-//
-//        System.out.println("In summary we pay: " + howMuchDoWePay + " yearly to all employees.");
-    }
 }
